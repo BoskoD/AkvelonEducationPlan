38d4b2e8e037786e47007a2f55d80ee5389bc455
Bosko <boskodanilovic@outlook.com>
2020-08-23 16:15:58 +0200

    Immutable class

---
+++ Immutable-Class/Immute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Immutable_Class
+{
+    class Immute
+    {
+        public string Test { get; }
+        public Immute(string test)
+        {
+            Test = test;
+        }
+
+        
+
+
+    }
+}
