fefa4e9795b6cbbb55eae3e8fe740d4bd5022b50
Bosko <boskodanilovic@outlook.com>
2020-08-23 16:52:46 +0200

    List example

---
+++ List_example/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace List_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string> { "Milica", "Bosko", "Stefan" };
+            names.Add("Lazar");
+            names.Add("Jovana");
+            names.Remove("Jovana");
+            foreach (var item in names)
+            {
+                Console.WriteLine($"Hello {item.ToUpper()}!");
+            }
+
+            Console.WriteLine(names[1].ToLower());
+
+            var index = names.IndexOf("Bosko");
+            //var index = names.IndexOf("Scott");
+            if (index == -1)
+            {
+                Console.WriteLine($"When an index is not found, IndexOf returns {index} \n");
+            }
+            else
+            {
+                Console.WriteLine($"The name {names[index]} is at index: {index} \n");
+            }
+
+            names.Sort();
+            Console.WriteLine("Sorted list".ToUpper() + "\n");
+
+            foreach (var item in names)
+            {
+                Console.WriteLine(item.ToUpper());
+            }
+        }
+    }
+}
